b0af004c76b3187de9109b8753d5b459d1df938c
justinformentin <justin.formentin@gmail.com>
2020-04-09 22:19:34 -0400

    Changed useEffect to function to prevent element updating twice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import WidgetBlock from "./components/WidgetBlock";
 import { SidebarContents } from "./components/SidebarContents";
 import { updateWidgetStyle } from "./lib/utils";
@@ -9,25 +9,22 @@ import "./App.css";
 
 export default function App({ s }) {
   const [tab, setTab] = useState(0);
-  const [style, setStyle] = useState(null);
   const [state, setState] = useState({
     styleString: "",
     styleArr: []
   });
 
-  useEffect(() => {
-    if (style) {
-      let str = "";
-      const arr = [...state.styleArr];
-      const { selector, value } = style;
-      const idx = arr.findIndex(v => v.selector === selector);
-      idx === -1 ? arr.push({ selector, value }) : (arr[idx].value = value);
-      arr.map(v => (str += `${v.selector}:${v.value};`));
-      s.innerText = `body{${str}}`;
-      setState({ styleString: str, styleArr: arr });
-      updateWidgetStyle(style);
-    }
-  }, [style]);
+  const updateStyle = style => {
+    let str = "";
+    const arr = [...state.styleArr];
+    const { selector, value } = style;
+    const idx = arr.findIndex(v => v.selector === selector);
+    idx === -1 ? arr.push({ selector, value }) : (arr[idx].value = value);
+    arr.map(v => (str += `${v.selector}:${v.value};`));
+    s.innerText = `body{${str}}`;
+    setState({ styleString: str, styleArr: arr });
+    updateWidgetStyle(style);
+  };
 
   // const renderWidgets = () => (
   //   <livelike-widgets programid="91ca9edd-376a-4d27-a24a-6b607c15c17c">
@@ -44,7 +41,7 @@ export default function App({ s }) {
   return (
     <div className="outer-container">
       <SidebarContents
-        updateStyle={setStyle}
+        updateStyle={updateStyle}
         list={tab === 0 ? w : c}
         changeTab={setTab}
         tab={tab}
@@ -54,11 +51,11 @@ export default function App({ s }) {
         programid="91ca9edd-376a-4d27-a24a-6b607c15c17c"
       >
         {list.map((v, idx) => (
-          <WidgetBlock key={idx} id={idx} item={v} updateStyle={setStyle} />
+          <WidgetBlock key={idx} id={idx} item={v} updateStyle={updateStyle} />
         ))}
       </livelike-widgets>
       <livelike-chat
-      timestamps
+        timestamps
         class={tab === 1 ? "flex" : "hidden"}
         programid="91ca9edd-376a-4d27-a24a-6b607c15c17c"
       ></livelike-chat>
